6ba375b9598cdbae911fae4d1ec108825d66274d
VasiliPuchov <89031200106@mail.ru>
2022-10-18 17:47:35 +0300

    HomeWork1

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.*/
+/*
+int SecondNumber( int number){
+    int second = (number / 10) % 10; 
+    
+return second;
+}
+    
+Console.WriteLine("Введите  любое трехзначное число от 100 до 999 ");
+int YourNumber = Convert.ToInt32(Console.ReadLine());
+int resultat = SecondNumber(YourNumber);
+Console.WriteLine($"Вы ввели число {YourNumber} , вторая цифра вашего числа {resultat} ");
+*/
+
+/*
