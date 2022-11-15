9523313005cb77c473bfb4221249ed1244cda2a5
VasiliPuchov <89031200106@mail.ru>
2022-11-15 19:19:14 +0300

    Home work 6.1

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+   int[] Massiv(int size){
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    return array;
+}
+    void ShowArray(int[] array){
+        for(int i = 0; i < array.Length; i++){
+            Console.Write(array[i] + " ");
+        }
+    } 
+int Sum( int[] array){
+    int count = 0;
+    for( int i = 0; i < array.Length; i++){
+        if( array[i] > 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите какое количество чисел вы хотите ввезти ? : ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] My = Massiv(M);
+ShowArray(My);
+Console.WriteLine($"В вашем массиве из {M} элементов , всего {Sum(My)} положительных  ");
