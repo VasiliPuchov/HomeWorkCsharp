3645309d9564805772e790ab1cdb41f3fce1abd9
VasiliPuchov <89031200106@mail.ru>
2022-11-16 13:02:54 +0300

    Home work 6.2

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
 Задача 41: Пользователь вводит с клавиатуры M чисел.
  Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
-
+/*
    int[] Massiv(int size){
     int[] array = new int[size];
     for(int i = 0; i < size; i++)
@@ -28,3 +28,31 @@ int M = Convert.ToInt32(Console.ReadLine());
 int[] My = Massiv(M);
 ShowArray(My);
 Console.WriteLine($"В вашем массиве из {M} элементов , всего {Sum(My)} положительных  ");
+*/
+/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
+ double[] FindPunkt(int k1, int k2, int b1, int b2){
+    double x = (b2 -b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    double[] array = new double[2] ;
+    array[0] = x;
+    array[1] = y;
+ return array;
+ }
+   void ShowArray(double[] array){
+        for(int i = 0; i < array.Length; i++){
+            Console.Write( array[i] + " " ) ;
+        }
+    } 
+Console.WriteLine("Введите значение k1 : ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2 : ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b1 : ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2 : ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+double[] myArray = FindPunkt(k1, k2, b1, b2);
+ShowArray(myArray);
+
